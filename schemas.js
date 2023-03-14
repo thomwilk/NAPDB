d81ed0fbaa968f7be58c74e724b3d007acd38d83
thomwilk <thom.wilkinson@gmail.com>
2023-03-14 18:32:46 -0400

    1537

--- schemas.js
+++
@@ -1,4 +0,0 @@
-var { CreditModel } = require("./schemas/CreditSchema.cjs");
-var { EpisodeModel } = require("./schemas/EpisodeSchema.cjs");
-
-module.exports = { CreditModel, EpisodeModel };
--- views.js
+++ views.js
@@ -1,8 +1,8 @@
 const pug = require("pug");
-const indexFunction = pug.compileFile("./src/views/index.pug");
-const producerFunction = pug.compileFile("./src/views/producer.pug");
-const episodeFunction = pug.compileFile("./src/views/episode.pug");
-const searchFunction = pug.compileFile("./src/views/search.pug");
+const indexFunction = pug.compileFile("src/views/index.pug");
+const producerFunction = pug.compileFile("src/views/producer.pug");
+const episodeFunction = pug.compileFile("src/views/episode.pug");
+const searchFunction = pug.compileFile("src/views/search.pug");
 
 module.exports = {
   indexFunction,
